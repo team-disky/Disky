d4b8cf52a790c5b17e5aa7e006128f60780c76a9
wakasugishiho <athkrmkc@gmail.com>
2019-11-14 05:39:14 +0000

    [add]routing,view-page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :managers
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :manager do
+  	resources :products
+  end
 end
